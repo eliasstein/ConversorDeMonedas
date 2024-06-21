899e3c431568cfb670b636213a85ca72fe701d3c
eliasstein <eliasstein@users.noreply.github.com>
2024-06-21 18:42:05 -0300

    Request actualizado

--- src/com/oracle/conversorMonedas/Request.java
+++ src/com/oracle/conversorMonedas/Request.java
@@ -7,11 +7,14 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.logging.Logger;
 
 public class Request {
+    private String TOKEN="";
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://v6.exchangerate-api.com/v6/746b859df050e8736759e221/latest/USD"))
+            .uri(URI.create("https://v6.exchangerate-api.com/v6/"+TOKEN+"/latest/USD"))
             .build();
 
     public double getCurrencyRequest(String curren) {
@@ -22,10 +25,15 @@ public class Request {
         } catch (IOException | InterruptedException e) {
             System.out.println(e.getMessage());;
         }
-        if (response!=null) {
+        if (response.statusCode()==200) {
             Gson gson = new Gson();
             Currency currency = gson.fromJson(response.body(), Currency.class);
             return (double) currency.getConversionRate(curren);
+        }
+        else{
+            System.out.println("******************************************\n\n\nERROR la api no funciona");
+            System.out.println("El error puede ser que la pagina este caida");
+            System.out.println("O el token introducido sea invalido.\n\n\n******************************************");
         }
         return 0;
 
